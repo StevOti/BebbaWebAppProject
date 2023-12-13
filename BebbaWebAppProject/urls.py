a77e5271956acd3a7d292ab34b8a4a628973705b
StevOti <steveotieno5640@gmail.com>
2023-12-13 15:44:52 +0300

    Beba Website

--- BebbaWebAppProject/urls.py
+++ BebbaWebAppProject/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('about_us.html/', views.about, name='about'),
+    path('contact_us.html', views.contact, name='contact')
 ]
